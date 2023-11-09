010050879a5a7346fa53361369f7d3f01f0cb645
justindmeyer <justindmeyer@lewisu.edu>
2023-11-09 14:15:35 -0600

    update

--- TurtleDraw_jm.py
+++ TurtleDraw_jm.py
@@ -1,15 +1,16 @@
 import turtle
 
+TEXTFILE = "turtle-draw.txt"
+
 print("Starting Turtle Draw...")
 print()
 
-toddTheTurtle = turtle.Turtle()
-toddTheTurtle.forward(100)
-toddTheTurtle.left(90)
-toddTheTurtle.forward(100)
-toddTheTurtle.left(90)
-toddTheTurtle.forward(100)
-toddTheTurtle.left(90)
-toddTheTurtle.forward(100)
+turtleDrawTextFile = open(TEXTFILE, "r")
+line = turtleDrawTextFile.readline()
+while line:
+    print(line, end="")
+    line = turtleDrawTextFile.readline()
 
-turtle.done()
+print()
+print()
+print("End")
